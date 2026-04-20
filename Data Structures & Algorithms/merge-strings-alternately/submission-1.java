25cd6452391b9c9f0d3b800f948a8ae08e6ea623
Harsh Vardhan Singh <102163246+harshsingh7714@users.noreply.github.com>
2026-04-21 00:09:17 +0530

    Add: merge-strings-alternately - submission-1

---
+++ Data Structures & Algorithms/merge-strings-alternately/submission-1.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        int l1=word1.length();
+        int l2=word2.length();
+        int s=0;
+        char c1[]=word1.toCharArray();
+        char c2[]=word2.toCharArray();
+        String res="";
+        s=Math.min(l1,l2);
+        for(int i=0;i<s;i++){
+                res+=c1[i];
+                res+=c2[i];
+        }
+        if(l1>s){
+            res+=word1.substring(s,l1);
+        }
+        if(l2>s){
+         res+=word2.substring(s,l2); 
+        }
+        return res;
+    }
+}
