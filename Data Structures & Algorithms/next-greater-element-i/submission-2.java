fe91a55b4df406016b960f25c6bc8ca7aaeac2b4
Harsh Vardhan Singh <102163246+harshsingh7714@users.noreply.github.com>
2026-06-12 01:15:29 +0530

    Add: next-greater-element-i - submission-2

---
+++ Data Structures & Algorithms/next-greater-element-i/submission-2.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int res[]=new int[nums1.length];
+        for(int i=0;i<nums1.length;i++){
+            res[i]=-1;
+            for(int j=0;j<nums2.length;j++){
+                if(nums1[i]==nums2[j]){
+                    if((j+1)==nums2.length){
+                        res[i]=-1;
+                    }
+                    else{
+                        for(int k=j+1;k<nums2.length;k++){
+                            if(nums1[i]<nums2[k]){
+                                res[i]=nums2[k];
+                                break;
+                            }
+                        }
+                    }
+
+                }
+            }
+        }
+        return res;
+    }
+}
